fbb96fde1fa12e4287ef24a415c19422e2d3585a
Rafael Guerra <rafael.09guerra@hotmail.com>
2021-01-09 14:50:08 -0300

    test2

--- BullyAlgorithmImplemetation/util_methods.py
+++ BullyAlgorithmImplemetation/util_methods.py
@@ -12,14 +12,14 @@ def generate_node_id():
 
 # This method is used to register the service in the service registry
 def register_service(name, port, node_id):
-    url = "http://localhost:8500/v1/agent/service/register"
+    url = "http://75.101.254.15:8500/v1/agent/service/register"
     data = {
         "Name": name,
         "ID": str(node_id),
         "port": port,
         "check": {
             "name": "Check Counter health %s" % port,
-            "tcp": "localhost:%s" % port,
+            "tcp": "75.101.254.15:%s" % port,
             "interval": "10s",
             "timeout": "1s"
         }
@@ -30,7 +30,7 @@ def register_service(name, port, node_id):
 
 def check_health_of_the_service(service):
     print('Checking health of the %s' % service)
-    url = 'http://localhost:8500/v1/agent/health/service/name/%s' % service
+    url = 'http://75.101.254.15:8500/v1/agent/health/service/name/%s' % service
     response = requests.get(url)
     response_content = json.loads(response.text)
     aggregated_state = response_content[0]['AggregatedStatus']
@@ -67,7 +67,7 @@ def get_higher_nodes(node_details, node_id):
 def election(higher_nodes_array, node_id):
     status_code_array = []
     for each_port in higher_nodes_array:
-        url = 'http://localhost:%s/proxy' % each_port
+        url = 'http://75.101.254.15:%s/proxy' % each_port
         data = {
             "node_id": node_id
         }
@@ -99,7 +99,7 @@ def ready_for_election(ports_of_all_nodes, self_election, self_coordinator):
 def get_details(ports_of_all_nodes):
     node_details = []
     for each_node in ports_of_all_nodes:
-        url = 'http://localhost:%s/nodeDetails' % ports_of_all_nodes[each_node]
+        url = 'http://75.101.254.15:%s/nodeDetails' % ports_of_all_nodes[each_node]
         data = requests.get(url)
         node_details.append(data.json())
     return node_details
@@ -112,6 +112,6 @@ def announce(coordinator):
         'coordinator': coordinator
     }
     for each_node in all_nodes:
-        url = 'http://localhost:%s/announce' % all_nodes[each_node]
+        url = 'http://75.101.254.15:%s/announce' % all_nodes[each_node]
         print(url)
         requests.post(url, json=data)
